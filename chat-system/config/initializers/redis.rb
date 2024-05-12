8330a2d4beea301210ca08a350209e05c4a44de5
Mohamed Shetewi <muhamedshetewi@gmail.com>
2024-05-12 17:34:25 +0300

    add redis

--- chat-system/config/initializers/redis.rb
+++ chat-system/config/initializers/redis.rb
@@ -1,5 +1,12 @@
 require 'redis'
 
+# Create a new Redis client
+redis = Redis.new(url: ENV['REDIS_URL'])
 
-# Connect to Redis
-$redis = Redis.new(url: ENV["REDIS_URL"])
+# Test the connection
+begin
+    redis.ping
+    puts "Successfully connected to Redis!"
+rescue Redis::CannotConnectError => e
+    puts "Failed to connect to Redis: #{e.message}"
+end
