dc371fdbb4dbb15287cb65a6b3415e00a743407a
Mohamed Shetewi <muhamedshetewi@gmail.com>
2024-05-14 00:55:06 +0300

    add foreign keys

--- chat-system/app/workers/create_message_worker.rb
+++ chat-system/app/workers/create_message_worker.rb
@@ -2,6 +2,13 @@ class CreateMessageWorker
   include Sidekiq::Worker
 
   def perform(app_id, chat_id, message_id, message)
-    Message.create(appID: app_id, chatID: chat_id, messageID: message_id, message: message)
+    new_message = Message.new(appID: app_id, chatID: chat_id, messageID: message_id, message: message)
+    if new_message.save
+      # Record was successfully saved
+      puts "Record saved successfully!"
+    else
+      # Failed to save the record
+      puts "Failed to save record: #{new_message.errors.full_messages.join(', ')}"
+    end
   end
 end
