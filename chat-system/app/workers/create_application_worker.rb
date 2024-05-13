18b3d9361d6e8ce235281fc9d15b60f7c305c1f4
Mohamed Shetewi <muhamedshetewi@gmail.com>
2024-05-13 17:47:26 +0300

    Add applications apis with caching

--- chat-system/app/workers/create_application_worker.rb
+++ chat-system/app/workers/create_application_worker.rb
@@ -4,6 +4,7 @@ class CreateApplicationWorker
   
     def perform(app_id, username)
       Application.create(appID: app_id, username: username, chatsCount: 0)
+      puts "finished creation of entry in db with ID: #{app_id} and Username: #{username}"
     end
 
   end
